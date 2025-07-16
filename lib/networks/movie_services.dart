996c3d8cd7d41c62f076e0d68b62e90ec8bd12a4
kien <anhkien7780@gmail.com>
2025-07-16 10:04:15 +0700

    feat: add refresh movies

--- lib/networks/movie_services.dart
+++ lib/networks/movie_services.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:math';
 
 import 'package:http/http.dart';
 
@@ -30,6 +31,37 @@ class MovieServices {
     }).toList();
     return await Future.wait(futures);
   }
+
+  static Future<List<Movie>> fetchRandomMovies() async {
+    final random = Random();
+    final randomPage = random.nextInt(500) + 1; // TMDb hỗ trợ đến 500 trang
+
+    final uri = Uri.parse(
+      "https://api.themoviedb.org/3/movie/top_rated?api_key=$_apiKey&page=$randomPage",
+    );
+
+    final response = await get(uri);
+    if (response.statusCode != 200) throw Exception("Failed to fetch random movie list");
+
+    final rawData = jsonDecode(response.body) as Map<String, dynamic>;
+    final List results = rawData["results"];
+
+    final futures = results.map<Future<Movie>>((movie) async {
+      final movieId = movie["id"];
+      final detailUri = Uri.parse(
+        "https://api.themoviedb.org/3/movie/$movieId?api_key=$_apiKey",
+      );
+      final detailRes = await get(detailUri);
+      if (detailRes.statusCode == 200) {
+        return Movie.fromJson(jsonDecode(detailRes.body));
+      } else {
+        throw Exception("Failed to fetch movie id $movieId");
+      }
+    }).toList();
+
+    return await Future.wait(futures);
+  }
+
 
   static Future<Movie> fetchMovie(int id) async{
     Uri uri = Uri.parse(
--- lib/screens/movies_screen.dart
+++ lib/screens/movies_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
 import 'package:movie_app/networks/movie_services.dart';
 import 'package:provider/provider.dart';
 
@@ -20,7 +19,9 @@ class _MoviesScreenState extends State<MoviesScreen> {
   @override
   void initState() {
     super.initState();
-    _moviesFuture = MovieServices.fetchMovies();
+    setState(() {
+      _moviesFuture = MovieServices.fetchMovies();
+    });
   }
 
   @override
@@ -28,80 +29,88 @@ class _MoviesScreenState extends State<MoviesScreen> {
     return Scaffold(
       backgroundColor: Color(0xff242A32),
       body: SafeArea(
-        child: ListView(
-          addAutomaticKeepAlives: false,
-          children: [
-            Center(
-              child: Padding(
-                padding: EdgeInsets.only(top: 10, bottom: 20),
+        child: RefreshIndicator(
+          onRefresh: () async {
+            setState(() {
+              _moviesFuture = MovieServices.fetchRandomMovies();
+            });
+          },
+          child: ListView(
+            addAutomaticKeepAlives: false,
+            children: [
+              Center(
                 child: Padding(
-                  padding: EdgeInsets.symmetric(vertical: 8, horizontal: 134.5),
-                  child: Text(
-                    "Movies",
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontFamily: "Montserrat",
-                      fontWeight: FontWeight.w600,
-                      color: Color(0xffECECEC),
+                  padding: EdgeInsets.only(top: 10, bottom: 20),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                      vertical: 8,
+                      horizontal: 134.5,
+                    ),
+                    child: Text(
+                      "Movies",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontFamily: "Montserrat",
+                        fontWeight: FontWeight.w600,
+                        color: Color(0xffECECEC),
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            FutureBuilder(
-              future: _moviesFuture,
-              builder: (context, snapshot) {
-                final children = <Widget>[];
-                if(snapshot.hasData) {
-                  final movies = snapshot.data!;
-                  children.addAll(
-                    List.generate(movies.length, (index){
-                      final movie = movies[index];
-                      return GestureDetector(
-                        onTap: () {
-                          context.read<AppViewModel>().setSelectedMovieId(movie.id);
-                          Navigator.pushNamed(context, "/movie_detail");
-                        },
-                        child: Padding(
-                          padding: const EdgeInsets.only(
-                            top: 24,
-                            left: 24,
-                            right: 24,
+              FutureBuilder(
+                future: _moviesFuture,
+                builder: (context, snapshot) {
+                  final children = <Widget>[];
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    children.add(
+                      const Center(child: CircularProgressIndicator()),
+                    );
+                  }
+                  if (snapshot.hasError) {
+                    children.add(
+                      const Center(child: Text("Something went wrong")),
+                    );
+                  }
+                  if (snapshot.hasData) {
+                    final movies = snapshot.data!;
+                    children.addAll(
+                      List.generate(movies.length, (index) {
+                        final movie = movies[index];
+                        return GestureDetector(
+                          onTap: () {
+                            context.read<AppViewModel>().setSelectedMovieId(
+                              movie.id,
+                            );
+                            Navigator.pushNamed(context, "/movie_detail");
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.only(
+                              top: 24,
+                              left: 24,
+                              right: 24,
+                            ),
+                            child: MovieItem(
+                              title: movie.originalTitle,
+                              voteRate: movie.voteAverage.toString(),
+                              genre: movie.genres[0],
+                              runTime: movie.runTime.toString(),
+                              posterPath: movie.posterPath,
+                              releaseDate: movie.releaseDate.split("-")[0],
+                            ),
                           ),
-                          child: MovieItem(
-                            title: movie.originalTitle,
-                            voteRate: movie.voteAverage.toString(),
-                            genre: movie.genres[0],
-                            runTime: movie.runTime.toString(),
-                            posterPath: movie.posterPath,
-                            releaseDate: movie.releaseDate.split("-")[0],
-                          ),
-                        ),
-                      );
-                    })
+                        );
+                      }),
+                    );
+                  }
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: children,
                   );
-                }
-                if(snapshot.connectionState == ConnectionState.waiting) {
-                  children.add(
-                    const Center(
-                      child: CircularProgressIndicator(),
-                    ),
-                  );
-                }
-                if(snapshot.hasError) {
-                  children.add(
-                    const Center(
-                      child: Text("Something went wrong"),
-                    ),
-                  );
-                }
-                return Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: children,
-                );
-              },
-            ),
-          ],
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
